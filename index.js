6143bb81afd62838894015926c696b6fe7417798
Mohitsingh <142308041+mohit-100@users.noreply.github.com>
2024-03-02 15:25:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ app.use("/api/messages", messageRoute); // assuming messageRoute handles message
 );
 const io = socket(server, {
   cors: {
-     origin: ["https://chat-9rs33qmuu-mohit-100s-projects.vercel.app", "https://chat-app-iota-pied.vercel.app"],
+     allowedHeaders:["*"]
+     origin: "*",
     credentials: true,
   },
 });
